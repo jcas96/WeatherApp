24c0c02ee686cba8999f1ba111ce1c628302a348
jcas96 <162757478+jcas96@users.noreply.github.com>
2024-08-06 15:19:17 -0700

    Add files via upload

--- WeatherAppGUI/src/WeatherAppGUI.java
+++ WeatherAppGUI/src/WeatherAppGUI.java
@@ -1,7 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class WeatherAppGUI extends JFrame {
     public WeatherAppGUI(){
+        super("Weather App");
 
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(450, 650);
+
+
+        setLocationRelativeTo(null);
+
+        setLayout(null);
+
+        setResizable(false);
+
+
+        addGuiComponents();
+    }
+    private void addGuiComponents(){
+        // search field for your location
+
+        JTextField searchTextField = new JTextField();
+
+        //set your placement and size of search bar
+
+        searchTextField.setBounds(15,15,351,45);
+
+        //change font
+
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN,24));
     }
 }
